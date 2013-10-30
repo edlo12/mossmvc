faa1c257f6059c4d38990f5f2733132862605e4f
Edith López <lcedith@hotmail.com>
2013-10-30 22:47:04 +0100

    rättelser

--- themes/core/functions.php
+++ themes/core/functions.php
@@ -10,8 +10,8 @@
 <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">css3</a>
 <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css21">css21</a>
 <a href="http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance">unicorn</a>
-<a href="http://validator.w3.org/checklink?uri={$ly->request->current_url}">links</a>
-<a href="http://qa-dev.w3.org/i18n-checker/index?async=false&amp;docAddr={$ly->request->current_url}">i18n</a>
+<a href="http://validator.w3.org/checklink?uri={$moss->request->current_url}">links</a>
+<a href="http://qa-dev.w3.org/i18n-checker/index?async=false&amp;docAddr={$moss->request->current_url}">i18n</a>
 <!-- <a href="link?">http-header</a> -->
 <a href="http://csslint.net/">css-lint</a>
 <a href="http://jslint.com/">js-lint</a>
