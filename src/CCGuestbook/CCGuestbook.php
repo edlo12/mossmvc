60fd330984b5ed80b56113b155740c5f2317733d
Edith López <lcedith@hotmail.com>
2013-11-28 06:03:19 +0100

    Separera HTML

--- src/CCGuestbook/CCGuestbook.php
+++ src/CCGuestbook/CCGuestbook.php
@@ -22,57 +22,19 @@ class CCGuestbook extends CObject implements IController, IHasSQL {
 
       /**
        * Implementing interface IController. All controllers must have an index action.
-       
-      public function Index() {   
-        $this->data['title'] = $this->pageTitle;
-        $this->data['main'] = $this->pageHeader . $this->pageForm;
-      }*/
-      /**
-       * Implementing interface IController. All controllers must have an index action.
        */
-      public function Index() {   
-        $formAction = $this->request->CreateUrl('guestbook/handler');
-        $this->pageForm = "
-          <form action='{$formAction}' method='post'>
-            <p>
-              <label>Message: <br/>
-              <textarea name='newEntry'></textarea></label>
-            </p>
-            <p>
-              <input type='submit' name='doAdd' value='Add message' />
-              <input type='submit' name='doClear' value='Clear all messages' />
-              <input type='submit' name='doCreate' value='Create database' />
-            </p>
-          </form>
-        ";
-        $this->data['title'] = $this->pageTitle;
-        $this->data['main']  = $this->pageHeader . $this->pageForm . $this->pageMessages;
-       
-    //    if(isset($_SESSION['guestbook'])) {
-    //      foreach($_SESSION['guestbook'] as $val) {
-    //        $this->data['main'] .= "<div style='background-color:#f6f6f6;border:1px solid #ccc;margin-bottom:1em;padding:1em;'><p>At: {$val['time']}</p><p>{$val['entry']}</p></div>\n";
-    //      }
-    //    }
-         $entries = $this->ReadAllFromDatabase();
-        foreach($entries as $val) {
-          $this->data['main'] .= "<div style='background-color:#f6f6f6;border:1px solid #ccc;margin-bottom:1em;padding:1em;'><p>At: {$val['created']}</p><p>" . htmlent($val['entry']) . "</p></div>\n";
-        }
+      public function Index() {
+            $this->data['title'] = $this->pageTitle;
+
+    // Include the file and store it in a string using output buffering
+    $entries = $this->ReadAllFromDatabase();
+    $formAction = $this->request->CreateUrl('guestbook/handler');
+    ob_start();
+    include __DIR__ . '/index.tpl.php';
+    $this->data['main'] = ob_get_clean();
+    
       }
-      /**
-       * Add a entry to the guestbook.
-      
-      public function Add() {
-        if(isset($_POST['doAdd'])) {
-          $entry = strip_tags($_POST['newEntry']);
-          $time = date('r');
-          $_SESSION['guestbook'][] = array('time'=>$time, 'entry'=>$entry);
-        }
-        elseif(isset($_POST['doClear'])) {
-          unset($_SESSION['guestbook']);
-        }           
-        header('Location: ' . $this->request->CreateUrl('guestbook'));
-      }
- */
+    
       /**
        * Handle posts from the form and take appropriate action.
        */
