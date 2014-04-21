1b72e1ee5bc87e177c2f551aa3790c203c807546
Edith López <lcedith@hotmail.com>
2014-04-21 11:25:17 +0200

    Spara allt, filtrera och formattera vid utskrift

--- src/CCContent/index.tpl.php
+++ src/CCContent/index.tpl.php
@@ -5,7 +5,7 @@
     <?php if($contents != null):?>
       <ul>
       <?php foreach($contents as $val):?>
-        <li><?=$val['id']?>, <?=$val['title']?> by <?=$val['owner']?> <a href='<?=create_url("content/edit/{$val['id']}")?>'>edit</a><a href='<?=create_url("page/view/{$val['id']}")?>'>view</a>
+        <li><?=$val['id']?>, <?=$val['title']?> by <?=$val['owner']?> <a href='<?=create_url("content/edit/{$val['id']}")?>'>edit</a> <a href='<?=create_url("page/view/{$val['id']}")?>'> view</a>
       <?php endforeach; ?>
       </ul>
     <?php else:?>
