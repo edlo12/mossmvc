bd23be3bafabbe48816e11cfeb461d79ae8dcfca
Edith López <lcedith@hotmail.com>
2014-04-22 20:47:32 +0200

    filtrera innehåll med HTMLPurifier

--- src/CCBlog/index.tpl.php
+++ src/CCBlog/index.tpl.php
@@ -7,7 +7,7 @@
         <p class='smaller-text'><em>Posted on <?=$val['created']?> by <?=$val['owner']?></em></p>
         <p><?=filter_data($val['data'], $val['filter'])?></p>
         <p class='smaller-text silent'><a href='<?=create_url("content/edit/{$val['id']}")?>'>edit</a></p>
-      <?php endforeach; ?>
+      <?p hp endforeach; ?>
     <?php else:?>
       <p>No posts exists.</p>
     <?php endif;?>
